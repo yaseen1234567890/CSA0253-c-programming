0a07ccebe5f46e191efbaf218a193420cf5bbe1e
A.MOHAMMED YASEEN (192121129) <112368880+yaseen1234567890@users.noreply.github.com>
2023-03-22 08:56:51 +0530

    Add files via upload

---
+++ pattern of number.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+	int i,j,n;
+	printf("Enter number of rows:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+
+	{
+		for(j=n;j>i;j--)
+
+		{
+			printf(" ");
+
+		}
+		for(j=i;j>=1;j--)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+
+	}
+	return 0;
+}
