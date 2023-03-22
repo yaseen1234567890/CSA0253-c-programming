2cd90c163600f375752db3a5762177edc926aabf
A.MOHAMMED YASEEN (192121129) <112368880+yaseen1234567890@users.noreply.github.com>
2023-03-22 09:03:41 +0530

    Add files via upload

---
+++ assignment2 5th.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int i,j,n;
+	printf("Enter number of rows:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+
+	{
+		for(j=n;j>i;j--)
+
+		{
+			printf(" ");
+
+		
+		
+               
+		
+			printf("%d ",j);
+
+		}
+
+	printf("\n");
+
+	}
+	return 0;
+}
+
